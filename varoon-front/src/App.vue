9a49e3dc3460bf0b9f71b97f2da95a18254cb8e0
timepresent95 <timepresent95@gmailcom>
2023-10-30 02:00:55 +0900

    Feat: 비정상적인 문법을 사용중인 코드 제거

--- varoon-front/src/App.vue
+++ varoon-front/src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <span class="fontDown1">가</span>
-    <span class="fontDown2">가</span>
-    <span class="fontDown3">가</span>
-    <span class="fontDown4">가</span>
     <TopNavi />
     <router-view class="container"></router-view>
     <Footer />
@@ -31,49 +27,6 @@ export default {
   padding: 0px;
   box-sizing: border-box;
 }
-@font-face {
-  font-family: "NanumBarunGothicOTF";
-  src: url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothic.woff2")
-      format("woff2"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothic.woff")
-      format("woff"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothic.ttf")
-      format("truetype");
-  font-style: normal;
-}
-
-@font-face {
-  font-family: "NanumBarunGothicUltraLightOTF";
-  src: url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicUltraLight.woff2")
-      format("woff2"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicUltraLight.woff")
-      format("woff"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicUltraLight.ttf")
-      format("truetype");
-  font-style: normal;
-}
-
-@font-face {
-  font-family: "NanumBarunGothicLightOTF";
-  src: url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicLight.woff2")
-      format("woff2"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicLight.woff")
-      format("woff"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicLight.ttf")
-      format("truetype");
-  font-style: normal;
-}
-
-@font-face {
-  font-family: "NanumBarunGothicBoldOTF";
-  src: url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicBold.woff2")
-      format("woff2"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicBold.woff")
-      format("woff"),
-    url("https://varoon.s3.ap-northeast-2.amazonaws.com/Fonts/NanumBarunGothicBold.ttf")
-      format("truetype");
-  font-style: normal;
-}
 body,
 #app {
   width: 100%;
@@ -93,32 +46,5 @@ ul, li, ol {
 a {
   text-decoration: none;
 }
-.fontDown1 {
-  cursor: default;
-  position: absolute;
-  color: rgba(255, 255, 255, 1);
-  font-family: NanumBarunGothicUltraLightOTF;
-  font-size: 20px;
-}
-.fontDown2 {
-  cursor: default;
-  position: absolute;
-  color: rgba(255, 255, 255, 1);
-  font-family: NanumBarunGothicOTF;
-  font-size: 20px;
-}
-.fontDown3 {
-  cursor: default;
-  position: absolute;
-  color: rgba(255, 255, 255, 1);
-  font-family: NanumBarunGothicLightOTF;
-  font-size: 20px;
-}
-.fontDown4 {
-  cursor: default;
-  position: absolute;
-  color: rgba(255, 255, 255, 1);
-  font-family: NanumBarunGothicBoldOTF;
-  font-size: 20px;
-}
+
 </style>
