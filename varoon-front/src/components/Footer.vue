9e7bd4ac538b64471c9aeaee402a62856cc8c437
timepresent95 <timepresent95@gmailcom>
2023-11-02 18:50:19 +0900

    Feat: Footer 상단 여백 간격 추가

--- varoon-front/src/components/Footer.vue
+++ varoon-front/src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="footer text-w1 text-right py-12 px-40">
+  <footer class="footer text-w1 text-right py-12 px-40 mt-120">
     <p class="text-h1">Give You A New Light</p>
     <p class="text-body1">새로운 빛을 선물합니다</p>
   </footer>
