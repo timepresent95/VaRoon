ff32aa3e10911cbc2dfde1c63f69839993cfa0c2
timepresent95 <timepresent95@gmailcom>
2023-11-05 17:09:16 +0900

    Feat: 에러 페이지 수정

--- varoon-front/src/view/Error404.vue
+++ varoon-front/src/view/Error404.vue
@@ -1,60 +1,30 @@
 <template>
-  <div class="error-content">
-    <div class="image-404-content">
-      <img class="image-404" src="@/images/error-illust-img@2x.png" />
-    </div>
-    <p class="error-text">찾을 수 없는 페이지를 요청하셨습니다.</p>
-    <router-link to="/">
-      <div class="error-home-btn">Home</div>
+  <div class="error-404 mbox pt-60 px-16">
+    <img src="@/images/error-404.png" alt="404 error"/>
+    <p class="text-h2b text-center mt-40">찾을 수 없는 페이지를 요청하셨습니다</p>
+    <router-link to="/" custom v-slot="{navigate}">
+      <button @click="navigate" class="go-home-button text-body1 text-w1 py-8 px-12 mt-40 mx-auto">
+        메인 화면으로
+      </button>
     </router-link>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {};
+<style scoped lang="scss">
+.error-404 {
+  img {
+    width: 100%;
+    max-width: 380px;
+    object-fit: contain;
   }
-};
-</script>
-<style>
-.error-content {
-  width: 100%;
-  height: calc(100vh - 191px);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 10rem;
-}
-.image-404-content {
-  width: 822px;
-  height: 411px;
-}
-.image-404 {
-  width: 822px;
-  height: 411px;
-  object-fit: contain;
-}
-.error-text {
-  height: 32px;
-  font-family: NanumBarunGothicOTF;
-  font-size: 28px;
-  text-align: center;
-  color: #000000;
-  margin: 20px 0;
-}
-.error-home-btn {
-  width: 416px;
-  height: 64px;
-  border-radius: 3px;
-  box-shadow: 0 3px 10px 0 rgba(75, 116, 255, 0.6);
-  background-color: #4b74ff;
-  font-family: NanumBarunGothicOTF;
-  font-size: 20px;
-  line-height: 64px;
-  outline: none;
-  text-align: center;
-  color: #ffffff;
-  cursor: pointer;
+
+  .go-home-button {
+    display: block;
+    width: 380px;
+    background-color: $p1;
+    border-radius: 4px;
+    outline: none;
+    cursor: pointer;
+    border: none;
+  }
 }
 </style>
