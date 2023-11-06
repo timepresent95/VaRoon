8eb2c30c134aceebb4f49a5426270f6e2efa5cdf
timepresent95 <timepresent95@gmail.com>
2023-11-06 20:52:34 +0900

    Feat: VR 마켓 내부 탑 네비게이션 공통 컴포넌트로 추출

--- varoon-front/src/components/Common/InnerTopNavigation.vue
+++ varoon-front/src/components/Common/InnerTopNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navigation">
-    <router-link v-for="{name, title} in navigationInfo" :key="name" :to="{name}"
+    <router-link v-for="{name, title} in paths" :key="name" :to="{name}"
                  class="text-h4b px-4 pb-4" :class="{'text-p1 current-link': name===route.name}">
       {{ title }}
     </router-link>
@@ -9,14 +9,9 @@
 <script setup>
 import {useRoute} from 'vue-router'
 
+defineProps({paths: {required: true, type: Array}})
 const route = useRoute();
 
-const navigationInfo = [
-  {name: 'vr-market-search', title: 'VR 마켓'},
-  {name: 'vr-market-cart', title: '장바구니'},
-  {name: 'vr-market-library', title: '구매내역'},
-  {name: 'vr-market-client-download', title: '클라이언트 다운로드'},
-]
 
 </script>
 <style scoped lang="scss">
--- varoon-front/src/view/VRMarket/index.vue
+++ varoon-front/src/view/VRMarket/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="vr-market mbox pt-12 px-16">
-    <navigation class="pb-12"/>
+    <inner-top-navigation :paths="paths" class="pb-12"/>
     <router-view/>
   </div>
 </template>
 <script setup>
-import Navigation from '@/components/VRMarket/Navigation';
+import InnerTopNavigation from '@/components/Common/InnerTopNavigation';
+
+const paths = [
+  {name: 'vr-market-search', title: 'VR 마켓'},
+  {name: 'vr-market-cart', title: '장바구니'},
+  {name: 'vr-market-library', title: '구매내역'},
+  {name: 'vr-market-client-download', title: '클라이언트 다운로드'},
+]
 </script>
 <style scoped lang="scss">
 .vr-market {
