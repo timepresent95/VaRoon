0d7b2e374f1281588b9c4a59272cd7fed05c1570
timepresent95 <timepresent95@gmail.com>
2023-11-07 17:42:18 +0900

    Chore: User Mock 데이터 오타 수정

--- varoon-front/src/assets/mock/user.js
+++ varoon-front/src/assets/mock/user.js
@@ -1,20 +1,20 @@
 export const doctorUser = {
   name: '테스트 의사',
-  birthday: {year: '2023', month: '12', dat: '01'},
+  birthday: {year: '2023', month: '12', date: '01'},
   gender: 'female',
   email: 'doctor@test.com',
   type: 'doctor'
 }
 export const patientUser = {
   name: '테스트 환자',
-  birthday: {year: '2023', month: '12', dat: '01'},
+  birthday: {year: '2023', month: '12', date: '01'},
   gender: 'male',
   email: 'patient@test.com',
   type: 'patient'
 }
 export const sellerUser = {
   name: '테스트 판매자',
-  birthday: {year: '2023', month: '12', dat: '01'},
+  birthday: {year: '2023', month: '12', date: '01'},
   gender: 'seller',
   email: 'test@test.com',
   type: 'seller'
